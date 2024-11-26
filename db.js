f253271c642d24e5a62a864704f76fa42436862b
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-11-26 09:39:45 +0600

    Update db.js to use environment variables for database connection, add dotenv dependency to package.json and package-lock.json

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "raka",
-    database: "notum",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log("Error when connecting to DB:", err);
-    }
-    console.log("Connected to the database.");
-    console.log("Open your browser and go to http://localhost:3000");
+  if (err) {
+    console.log("Error when connecting to DB:", err);
+  }
+  console.log("Connected to the database.");
+  console.log("Open your browser and go to http://localhost:3000");
 });
 
 module.exports = connection;
