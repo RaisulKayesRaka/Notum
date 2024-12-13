ea7c1e52dd35ef0bfe1c744928612fcf578df884
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-12-13 08:19:42 +0600

    Fix sidemenu not working problem

--- notum.sql
+++ notum.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE notum;
+
+USE notum;
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
@@ -15,30 +19,22 @@ CREATE TABLE notes (
     is_deleted BOOLEAN DEFAULT FALSE,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at DATETIME NULL,
-    FOREIGN KEY (user_id)
-        REFERENCES users (id)
-        ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     category_name VARCHAR(100) NOT NULL,
-    FOREIGN KEY (user_id)
-        REFERENCES users (id)
-        ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE note_categories (
     note_id INT NOT NULL,
     category_id INT NOT NULL,
-    PRIMARY KEY (note_id , category_id),
-    FOREIGN KEY (note_id)
-        REFERENCES notes (id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (category_id)
-        REFERENCES categories (id)
-        ON DELETE CASCADE
+    PRIMARY KEY (note_id, category_id),
+    FOREIGN KEY (note_id) REFERENCES notes (id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
 
 CREATE TABLE todos (
@@ -46,68 +42,72 @@ CREATE TABLE todos (
     user_id INT NOT NULL,
     task VARCHAR(250) NOT NULL,
     is_completed BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (user_id)
-        REFERENCES users (id)
-        ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE VIEW active_notes AS
-    SELECT 
-        *
-    FROM
-        notes
-    WHERE
-        is_deleted = FALSE
-            AND is_archived = FALSE;
+SELECT
+    *
+FROM
+    notes
+WHERE
+    is_deleted = FALSE
+    AND is_archived = FALSE;
 
 CREATE VIEW archived_notes AS
-    SELECT 
-        *
-    FROM
-        notes
-    WHERE
-        is_archived = TRUE;
+SELECT
+    *
+FROM
+    notes
+WHERE
+    is_archived = TRUE;
 
 CREATE VIEW deleted_notes AS
-    SELECT 
-        *
-    FROM
-        notes
-    WHERE
-        is_deleted = TRUE;
+SELECT
+    *
+FROM
+    notes
+WHERE
+    is_deleted = TRUE;
 
 CREATE VIEW uncompleted_todos AS
-    SELECT 
-        id, user_id, task
-    FROM
-        todos
-    WHERE
-        is_completed = FALSE;
-        
+SELECT
+    id,
+    user_id,
+    task
+FROM
+    todos
+WHERE
+    is_completed = FALSE;
+
 CREATE VIEW completed_todos AS
-    SELECT 
-        id, user_id, task
-    FROM
-        todos
-    WHERE
-        is_completed = TRUE;
+SELECT
+    id,
+    user_id,
+    task
+FROM
+    todos
+WHERE
+    is_completed = TRUE;
 
 CREATE VIEW popular_categories AS
-SELECT 
+SELECT
     categories.id AS category_id,
     categories.category_name,
     COUNT(note_categories.note_id) AS note_count
-FROM 
+FROM
     categories
-LEFT JOIN 
-    note_categories ON categories.id = note_categories.category_id
-GROUP BY 
-    categories.id, categories.category_name
-ORDER BY 
+    LEFT JOIN note_categories ON categories.id = note_categories.category_id
+GROUP BY
+    categories.id,
+    categories.category_name
+ORDER BY
     note_count DESC
-LIMIT 6;
+LIMIT
+    6;
 
-CREATE 
-    TRIGGER  before_note_update
- BEFORE UPDATE ON notes FOR EACH ROW 
-    SET NEW . updated_at = NOW();
+CREATE TRIGGER before_note_update BEFORE
+UPDATE
+    ON notes FOR EACH ROW
+SET
+    NEW.updated_at = NOW();
